99832518330c15cc88c1bfc2c42c0d27fb343527
jstrickler <jstrickler@gmail.com>
2019-10-24 12:21:38 -0600

    copy range to sheet

--- EXAMPLES/px_save_range_to_sheet.py
+++ EXAMPLES/px_save_range_to_sheet.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 import openpyxl as px
 
+WB = '../DATA/presidents.xlsx'
 
 def main():
     """program entry point"""
-    wb = px.load_workbook('../DATA/presidents.xlsx')
+    wb = px.load_workbook(WB)
     ws = wb['US Presidents']
     ws2 = wb.create_sheet('President Names')
 
     save_range_to_new_sheet(ws, ws2)
 
-    wb.save()
+#    wb.save("presidents4.xlsx")
+    wb.close()
 
 
 def save_range_to_new_sheet(ws, ws2):
     """Print first and last names of all presidents"""
-    pres_range = ws['B2':'C46']  # cell range
 
-    for i, row in enumerate(pres_range, 1):
-        for j, col in enumerate(row, 1):
-            ws2.cell(row=i, column=j).value = ws.cell(row=i, column=j)
+    for row in ws['B2':'C46']:
+        for col in row:
+            ws2[col.coordinate] = col.value
 
 if __name__ == '__main__':
     main()
