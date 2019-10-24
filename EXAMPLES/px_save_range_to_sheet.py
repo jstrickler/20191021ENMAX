1a8dcdd9d689c30f309ce38a2f60a65bb7783cdb
jstrickler <jstrickler@gmail.com>
2019-10-24 12:51:56 -0600

    end of lunch

--- EXAMPLES/px_save_range_to_sheet.py
+++ EXAMPLES/px_save_range_to_sheet.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+"""
+Copy a range from an existing spreadsheet
+to a new sheet
+"""
 import openpyxl as px
 
 WB = '../DATA/presidents.xlsx'
@@ -8,14 +12,13 @@ def main():
     wb = px.load_workbook(WB)
     ws = wb['US Presidents']
     ws2 = wb.create_sheet('President Names')
-
-    save_range_to_new_sheet(ws, ws2)
-
-#    wb.save("presidents4.xlsx")
+    save_range_to_new_worksheet(ws, ws2)
     wb.close()
 
+# save as:
+#   wb.save("presidents4.xlsx")
 
-def save_range_to_new_sheet(ws, ws2):
+def save_range_to_new_worksheet(ws, ws2):
     """Print first and last names of all presidents"""
 
     for row in ws['B2':'C46']:
